178ef4732a63b1aeb56378aff713d410899750d4
devops430 <raparlab1@gmail.com>
2018-07-12 10:26:49 +0000

    created narahari and narasimha files

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#purpose : This is a shell script
+#version : 1.1
+#owner   : Narasimha
+#input 	 : none
+#output	 : to show welcome script
+
+ echo "welcome to the shell script"
+
+ sleep 10
+
+ echo "end of the script"
